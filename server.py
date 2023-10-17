5bcdec27b7bb9f329efb1f7e889ec49267cb3096
Ankit404butfound <ankitrajmahapatra@gmail.com>
2023-10-17 09:44:39 +0530

    Add webhook for github, add expense calculator

--- server.py
+++ server.py
@@ -172,6 +172,7 @@ def get_expenditure_by_person():
 @app.route("/listen_to_push_event", methods=["POST"])
 def listen_to_push_event():
     print("push event received")
+    os.system
     return "success"
 
 
